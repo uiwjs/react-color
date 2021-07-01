3dc77faa15007db67efcd72c650d24d4a29b78a3
jaywcjlove <398188662@qq.com>
2021-07-02 02:28:43 +0800

    fix(shade-slider): fix Shade color errors.

--- packages/color-shade-slider/src/index.tsx
+++ packages/color-shade-slider/src/index.tsx
@@ -13,11 +13,12 @@ export default React.forwardRef<HTMLDivElement, ShadeSliderProps>((props, ref) =
     <Alpha
       ref={ref}
       {...other}
-      hsva={{ h: hsva.h, s: 100, v: hsva.v, a: hsva.v / 100 }}
+      hsva={{ h: hsva.h, s: 100, v: hsva.v, a: 1 - hsva.v / 100 }}
       direction={direction}
       background={`linear-gradient(to ${direction === 'horizontal' ? 'right' : 'bottom'}, ${colorFrom}, rgb(0, 0, 0))`}
       onChange={(_, interaction) => {
-        onChange && onChange({ v: direction === 'horizontal' ? interaction.left * 100 : interaction.top * 100, s: 100 });
+        onChange &&
+          onChange({ v: direction === 'horizontal' ? 100 - interaction.left * 100 : 100 - interaction.top * 100, s: 100 });
       }}
     />
   );
