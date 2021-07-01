d64f69f4aa633192d608ff3a9381bee5a5058410
jaywcjlove <398188662@qq.com>
2021-07-01 13:33:21 +0800

    refactor(color-saturation): refactor component.

--- packages/color-saturation/src/index.tsx
+++ packages/color-saturation/src/index.tsx
@@ -1,107 +1,66 @@
-import React, { useState, useRef, useImperativeHandle } from 'react';
+import React from 'react';
 import { HsvaColor, hsvaToHslaString } from '@uiw/color-convert';
+import Interactive, { Interaction } from '@uiw/react-drag-event-interactive';
 import { Pointer } from './Pointer';
-import { calculateChange, preventDefaultMove, isTouch } from './utils';
-import { useEffect } from 'react';
-import { useCallback } from 'react';
 
 export * from '@uiw/color-convert';
 
 export interface ColorSaturationProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange'> {
   prefixCls?: string;
+  /** hsva => `{ h: 0, s: 75, v: 82, a: 1 }` */
   hsva: HsvaColor;
-  onChange: (newColor: HsvaColor) => void;
+  onChange?: (newColor: HsvaColor) => void;
 }
 
 export default React.forwardRef<HTMLDivElement, ColorSaturationProps>((props, ref) => {
-  const { prefixCls = 'w-color-saturation', className, hsva, onChange, ...other } = props;
-  const container = useRef<HTMLDivElement>(null);
-  const hasTouched = useRef(false);
-  const [isDragging, setDragging] = useState(false);
-
-  useImperativeHandle(ref, () => container.current!);
+  const { prefixCls = 'w-color-saturation', className, style, hsva, onChange, ...other } = props;
   const containerStyle: React.CSSProperties = {
     width: 200,
     height: 200,
+    ...style,
     position: 'relative',
     backgroundColor: `hsl(${hsva.h},100%, 50%)`,
   };
 
-  // Prevent mobile browsers from handling mouse events (conflicting with touch ones).
-  // If we detected a touch interaction before, we prefer reacting to touch events only.
-  const isValid = (event: MouseEvent | TouchEvent): boolean => {
-    if (hasTouched.current && !isTouch(event)) return false;
-    if (!hasTouched.current) hasTouched.current = isTouch(event);
-    return true;
+  const handleChange = (interaction: Interaction, event: MouseEvent | TouchEvent) => {
+    onChange &&
+      onChange({
+        h: hsva.h,
+        s: interaction.left * 100,
+        v: (1 - interaction.top) * 100,
+        a: hsva.a,
+        // source: 'hsv',
+      });
   };
 
-  const handleMove = useCallback((event: MouseEvent | TouchEvent) => {
-    preventDefaultMove(event);
-    // If user moves the pointer outside of the window or iframe bounds and release it there,
-    // `mouseup`/`touchend` won't be fired. In order to stop the picker from following the cursor
-    // after the user has moved the mouse/finger back to the document, we check `event.buttons`
-    // and `event.touches`. It allows us to detect that the user is just moving his pointer
-    // without pressing it down
-    const isDown = isTouch(event) ? event.touches.length > 0 : event.buttons > 0;
-
-    if (isDown && container.current) {
-      const rect = calculateChange(event as any, hsva, container.current!);
-      onChange && onChange(rect);
-    } else {
-      setDragging(false);
-    }
-  }, []);
-
-  const handleMoveEnd = useCallback(() => setDragging(false), []);
-
-  const toggleDocumentEvents = useCallback((state: boolean) => {
-    const toggleEvent = state ? window.addEventListener : window.removeEventListener;
-    toggleEvent(hasTouched.current ? 'touchmove' : 'mousemove', handleMove);
-    toggleEvent(hasTouched.current ? 'touchend' : 'mouseup', handleMoveEnd);
-  }, []);
-
-  useEffect(() => {
-    toggleDocumentEvents(isDragging);
-    return () => {
-      isDragging && toggleDocumentEvents(false);
-    };
-  }, [isDragging, toggleDocumentEvents]);
-
-  const handleMoveStart = useCallback((event: React.MouseEvent | React.TouchEvent) => {
-    preventDefaultMove(event.nativeEvent);
-    if (!isValid(event.nativeEvent)) return;
-
-    const rect = calculateChange(event as any, hsva, container.current!);
-    onChange && onChange(rect);
-    setDragging(true);
-  }, []);
-
   return (
-    <div
-      className={`${prefixCls} ${className || ''}`}
-      style={containerStyle}
-      ref={container}
-      {...other}
-      onMouseDown={handleMoveStart}
-      onTouchStart={handleMoveStart}
-    >
-      <div
+    <div className={`${prefixCls} ${className || ''}`} style={containerStyle} ref={ref} {...other}>
+      <Interactive
         style={{
-          inset: 0,
-          background: 'linear-gradient(to right, rgb(255, 255, 255), rgba(255, 255, 255, 0))',
           position: 'absolute',
+          inset: 0,
         }}
+        onMove={handleChange}
+        onDown={handleChange}
       >
         <div
           style={{
             inset: 0,
-            background: 'linear-gradient(to top, rgb(0, 0, 0), rgba(0, 0, 0, 0))',
+            background: 'linear-gradient(to right, rgb(255, 255, 255), rgba(255, 255, 255, 0))',
             position: 'absolute',
           }}
         >
-          <Pointer prefixCls={prefixCls} top={`${100 - hsva.v}%`} left={`${hsva.s}%`} color={hsvaToHslaString(hsva)} />
+          <div
+            style={{
+              inset: 0,
+              background: 'linear-gradient(to top, rgb(0, 0, 0), rgba(0, 0, 0, 0))',
+              position: 'absolute',
+            }}
+          >
+            <Pointer prefixCls={prefixCls} top={`${100 - hsva.v}%`} left={`${hsva.s}%`} color={hsvaToHslaString(hsva)} />
+          </div>
         </div>
-      </div>
+      </Interactive>
     </div>
   );
 });
