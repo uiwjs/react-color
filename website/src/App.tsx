5ab8add1dde33adea32ae72e003ee63b5cb2ef2c
jaywcjlove <398188662@qq.com>
2021-07-01 13:30:57 +0800

    website: add example.

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,26 +1,32 @@
 import React, { useState } from 'react';
 import ColorSaturation, { hsvaToHslaString } from '@uiw/react-color-saturation';
+import ColorAlpha from '@uiw/react-color-alpha';
 import logo from './logo.svg';
 import styles from './App.module.css';
 
-function App() {
-  const [hsva, setHsva] = useState({ h: 0, s: 75, v: 82, a: 1 });
+export default function App() {
+  const [hsva, setHsva] = useState({ h: 0, s: 0, v: 68, a: 1 });
   return (
     <div className={styles.app}>
       <header className={styles.header} style={{ backgroundColor: hsvaToHslaString(hsva) }}>
         <img src={logo} className={styles.logo} alt="logo" />
+        <h1>React Color</h1>
+        <p>Color picker component for React.</p>
       </header>
       <div className={styles.content}>
         <ColorSaturation
           hsva={hsva}
           onChange={(newColor) => {
-            newColor = { ...hsva, ...newColor };
-            setHsva({ ...newColor });
+            setHsva({ ...hsva, ...newColor, a: hsva.a });
+          }}
+        />
+        <ColorAlpha
+          hsva={hsva}
+          onChange={(newColor) => {
+            setHsva({ ...hsva, ...newColor });
           }}
         />
       </div>
     </div>
   );
 }
-
-export default App;
