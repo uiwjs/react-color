6a5a2f782d608363e408849f48c526165e0cfa22
jaywcjlove <398188662@qq.com>
2021-07-07 08:19:01 +0800

    test(drag-event-interactive): add test case.

--- test/drag-event-interactive.test.tsx
+++ test/drag-event-interactive.test.tsx
@@ -3,8 +3,9 @@
  */
 import React, { useState } from 'react';
 import TestRenderer from 'react-test-renderer';
+import { screen, render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Interactive, { clamp } from '../packages/drag-event-interactive/src';
+import Interactive, { clamp, isTouch } from '../packages/drag-event-interactive/src';
 
 it('Interactive', async () => {
   const MyComponent = () => {
@@ -27,4 +28,62 @@ it('clamp', () => {
   expect(clamp(-0.123)).toEqual(0);
   expect(clamp(0)).toEqual(0);
   expect(clamp(0.33)).toEqual(0.33);
+});
+
+it('Interactive mouseDown', async () => {
+  const { getByTitle } = render(
+    <div style={{ position: 'relative', width: 320, height: 16 }}>
+      <Interactive
+        title="test"
+        onMove={() => {
+          console.log('test');
+        }}
+        onDown={(offset, event) => {
+          expect(isTouch(event)).toBeFalsy();
+          expect(Object.keys(offset)).toEqual(expect.arrayContaining(['left', 'top']));
+        }}
+      >
+        <div>xxx</div>
+      </Interactive>
+    </div>,
+  );
+  getByTitle('test').focus();
+  fireEvent.mouseDown(getByTitle('test'), { clientX: 1 });
+  fireEvent.mouseMove(getByTitle('test'), { clientX: 24 });
+});
+
+it('Interactive mouseDown', async () => {
+  const { getByTitle } = render(
+    <div style={{ position: 'relative', width: 320, height: 16 }}>
+      <Interactive
+        title="test"
+        onMove={() => {
+          console.log('touchmove');
+        }}
+        onDown={(offset, event) => {
+          console.log('tonDownest  xxxx', isTouch(event));
+          expect(Object.keys(offset)).toEqual(expect.arrayContaining(['left', 'top']));
+        }}
+      >
+        <div>xxx</div>
+      </Interactive>
+    </div>,
+  );
+  const elm = getByTitle('test');
+  elm.focus();
+  // fireEvent(
+  //   elm,
+  //   new MouseEvent('touchstart', {
+  //     bubbles: true,
+  //     cancelable: true,
+  //   }),
+  // );
+  fireEvent.touchMove(document, {});
+  fireEvent(
+    elm,
+    new MouseEvent('touchMove', {
+      bubbles: true,
+      cancelable: true,
+    }),
+  );
 });
