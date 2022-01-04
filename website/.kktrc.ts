773c936e5147205dbe2a37cf767afcefe01a7488
jaywcjlove <398188662@qq.com>
2022-01-04 14:01:07 +0800

    chore(deps): update dependency kkt to v7.
    Upgrade react-scripts to v5, Support Webpack 5.x
    https://github.com/kktjs/kkt/issues/198

--- website/.kktrc.ts
+++ website/.kktrc.ts
@@ -13,25 +13,24 @@ export default (conf: Configuration, env: 'development' | 'production', options:
     }),
   );
 
-  conf.optimization = {
-    ...conf.optimization,
-    splitChunks: {
-      cacheGroups: {
-        reactvendor: {
-          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-          name: 'react-vendor',
-          chunks: 'all',
-        },
-        prismjs: {
-          test: /[\\/]node_modules[\\/](prismjs)[\\/]/,
-          name: 'prismjs-vendor',
-          chunks: 'async',
+  if (env === 'production') {
+    conf.optimization = {
+      ...conf.optimization,
+      splitChunks: {
+        cacheGroups: {
+          reactvendor: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: 'react-vendor',
+            chunks: 'all',
+          },
+          refractor: {
+            test: /[\\/]node_modules[\\/](refractor)[\\/]/,
+            name: 'refractor-vendor',
+            chunks: 'all',
+          },
         },
       },
-    },
-  };
-
-  if (env === 'production') {
+    };
     conf.output = { ...conf.output, publicPath: './' };
   }
 
