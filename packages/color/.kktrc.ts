65c1c21f21071045bfbace7f6e2c8a5fa6674450
jaywcjlove <398188662@qq.com>
2022-03-04 01:59:13 +0800

    fix: add bundle file output config.

---
+++ packages/color/.kktrc.ts
@@ -0,0 +1,24 @@
+import { LoaderConfOptions, WebpackConfiguration } from 'kkt';
+import lessModules from '@kkt/less-modules';
+
+export default (conf: WebpackConfiguration, env: 'production' | 'development', options: LoaderConfOptions) => {
+  conf = lessModules(conf, env, options);
+  if (options.bundle) {
+    conf.output!.library = '@uiw/react-color';
+    conf.externals = {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      },
+    };
+  }
+  return conf;
+};
