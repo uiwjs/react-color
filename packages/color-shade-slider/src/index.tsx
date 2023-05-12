6b156511e5730d675d815022854f3cbf9e96a5d3
jaywcjlove <398188662@qq.com>
2023-05-13 01:12:09 +0800

    fix: fix shadow slider issue. (#115)

--- packages/color-shade-slider/src/index.tsx
+++ packages/color-shade-slider/src/index.tsx
@@ -3,23 +3,25 @@ import { hsvaToHslaString } from '@uiw/color-convert';
 import Alpha, { AlphaProps } from '@uiw/react-color-alpha';
 
 export interface ShadeSliderProps extends Omit<AlphaProps, 'onChange'> {
-  onChange?: (newShade: { v: number; s: number }) => void;
+  onChange?: (newShade: { v: number }) => void;
 }
 
 const ShadeSlider = React.forwardRef<HTMLDivElement, ShadeSliderProps>((props, ref) => {
   const { prefixCls = 'w-color-saturation', className, onChange, direction = 'horizontal', hsva, ...other } = props;
-  const colorFrom = hsvaToHslaString(Object.assign({}, hsva, { a: 1, s: 100, v: 100 }));
+  const colorFrom = hsvaToHslaString({ ...hsva, a: 1, v: 100 });
   return (
     <Alpha
       ref={ref}
       {...other}
       className={`${prefixCls} ${className || ''}`}
-      hsva={{ h: hsva.h, s: 100, v: hsva.v, a: 1 - hsva.v / 100 }}
+      hsva={{ h: hsva.h, s: hsva.s, v: hsva.v, a: 1 - hsva.v / 100 }}
       direction={direction}
       background={`linear-gradient(to ${direction === 'horizontal' ? 'right' : 'bottom'}, ${colorFrom}, rgb(0, 0, 0))`}
       onChange={(_, interaction) => {
         onChange &&
-          onChange({ v: direction === 'horizontal' ? 100 - interaction.left * 100 : 100 - interaction.top * 100, s: 100 });
+          onChange({
+            v: direction === 'horizontal' ? 100 - interaction.left * 100 : 100 - interaction.top * 100,
+          });
       }}
     />
   );
