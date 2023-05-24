781dd9e304d8d21dcb462cfbb6902d0cd1528db2
jaywcjlove <398188662@qq.com>
2023-05-24 21:16:34 +0800

    fix(convert): fix getContrastingColor result issue.

--- packages/color-convert/src/getContrastingColor.ts
+++ packages/color-convert/src/getContrastingColor.ts
@@ -2,9 +2,9 @@ import { HsvaColor, color } from './';
 
 export const getContrastingColor = (str: string | HsvaColor) => {
   if (!str) {
-    return '#fff';
+    return '#ffffff';
   }
   const col = color(str);
   const yiq = (col.rgb.r * 299 + col.rgb.g * 587 + col.rgb.b * 114) / 1000;
-  return yiq >= 128 ? '#000' : '#fff';
+  return yiq >= 128 ? '#000000' : '#ffffff';
 };
