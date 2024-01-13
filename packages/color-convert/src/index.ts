12eaa947b68267f30a2ef02af57f4fbeefe7c6a2
jaywcjlove <398188662@qq.com>
2024-01-13 14:58:24 +0800

    fix(ColorConvert): fix circular dependencies issue. #137

--- packages/color-convert/src/index.ts
+++ packages/color-convert/src/index.ts
@@ -1,8 +1,3 @@
-import { validHex } from './utils';
-
-export * from './utils';
-export * from './getContrastingColor';
-
 const RGB_MAX = 255;
 const HUE_MAX = 360;
 const SV_MAX = 100;
@@ -305,3 +300,40 @@ export const color = (str: string | HsvaColor): ColorResult => {
   }
   return { rgb, hsl, hsv, rgba, hsla, hsva, hex, hexa };
 };
+
+export const getContrastingColor = (str: string | HsvaColor) => {
+  if (!str) {
+    return '#ffffff';
+  }
+  const col = color(str);
+  const yiq = (col.rgb.r * 299 + col.rgb.g * 587 + col.rgb.b * 114) / 1000;
+  return yiq >= 128 ? '#000000' : '#ffffff';
+};
+
+export const equalColorObjects = (first: ObjectColor, second: ObjectColor): boolean => {
+  if (first === second) return true;
+
+  for (const prop in first) {
+    // The following allows for a type-safe calling of this function (first & second have to be HSL, HSV, or RGB)
+    // with type-unsafe iterating over object keys. TS does not allow this without an index (`[key: string]: number`)
+    // on an object to define how iteration is normally done. To ensure extra keys are not allowed on our types,
+    // we must cast our object to unknown (as RGB demands `r` be a key, while `Record<string, x>` does not care if
+    // there is or not), and then as a type TS can iterate over.
+    if ((first as unknown as Record<string, number>)[prop] !== (second as unknown as Record<string, number>)[prop]) return false;
+  }
+
+  return true;
+};
+
+export const equalColorString = (first: string, second: string): boolean => {
+  return first.replace(/\s/g, '') === second.replace(/\s/g, '');
+};
+
+export const equalHex = (first: string, second: string): boolean => {
+  if (first.toLowerCase() === second.toLowerCase()) return true;
+
+  // To compare colors like `#FFF` and `ffffff` we convert them into RGB objects
+  return equalColorObjects(hexToRgba(first), hexToRgba(second));
+};
+
+export const validHex = (hex: string): boolean => /^#?([A-Fa-f0-9]{3,4}){1,2}$/.test(hex);
