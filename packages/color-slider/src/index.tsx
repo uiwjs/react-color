39a03480570fd602ae7a786af55d706fad81e82d
Omar K <kzrooey@gmail.com>
2025-06-17 23:32:23 -0500

    added support for black and white, removing hsl conversion and instead using rgba, plus new customColorShades prop

--- packages/color-slider/src/index.tsx
+++ packages/color-slider/src/index.tsx
@@ -3,26 +3,70 @@ import {
   ColorResult,
   color as handleColor,
   hexToHsva,
-  hsvaToHsla,
-  hsvaToHslString,
+  hsvaToRgbaString,
   validHex,
+  hsvaToRgba,
   HsvaColor,
-  hslStringToHsva,
+  rgbaStringToHsva,
 } from '@uiw/color-convert';
 
 export interface SliderProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'onChange' | 'color'> {
   prefixCls?: string;
   color?: string | HsvaColor;
   lightness?: number[];
+  customColorShades?: { color: string | HsvaColor; lightness: number[] }[];
   onChange?: (color: ColorResult, evn: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
+const hsvaCheck = (color?: string | HsvaColor): HsvaColor => {
+  return (typeof color === 'string' && validHex(color) ? hexToHsva(color) : color || {}) as HsvaColor;
+};
+
+const hsvaEqual = (c1: HsvaColor, c2: HsvaColor, lightnessArray?: number[]): boolean => {
+  // Check for exact match of all properties
+  const exactMatch = c1.h === c2.h && c1.s === c2.s && c1.v === c2.v && c1.a === c2.a;
+
+  // If there's an exact match, return true
+  if (exactMatch) return true;
+
+  // If no exact match and a lightness array exists, check if the base
+  // properties match and value is within 1 unit of any lightness value
+  if (lightnessArray) {
+    const baseMatch = c1.h === c2.h && c1.s === c2.s && c1.a === c2.a;
+    return baseMatch && lightnessArray.some((lightness) => Math.abs(c2.v - lightness) <= 1);
+  }
+
+  return false;
+};
+
 const Slider = React.forwardRef<HTMLDivElement, SliderProps>((props, ref) => {
-  const { prefixCls = 'w-color-slider', className, style, onChange, color, lightness = [80, 65, 50, 35, 20], ...other } = props;
-  const hsva = (typeof color === 'string' && validHex(color) ? hexToHsva(color) : color || {}) as HsvaColor;
-  const handleClick = (hslStr: string, evn: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    onChange && onChange(handleColor(hslStringToHsva(hslStr)), evn);
+  const {
+    prefixCls = 'w-color-slider',
+    className,
+    style,
+    onChange,
+    color,
+    customColorShades = [{ color: hexToHsva('#000000'), lightness: [14, 12, 10, 8, 6] }],
+    lightness = [80, 65, 50, 35, 20],
+    ...other
+  } = props;
+  const hsva = hsvaCheck(color);
+
+  // Find matching custom color and its lightness array
+  const matchingCustomColor = customColorShades.find((shade) => {
+    const customHsva = hsvaCheck(shade.color);
+    const isMatch = hsvaEqual(customHsva, hsva, shade.lightness);
+    return isMatch;
+  });
+
+  // Determine which lightness array to use
+  const activeLightness = matchingCustomColor ? matchingCustomColor.lightness : lightness;
+
+  const handleClick = (rgbaStr: string, evn: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const newHsva = rgbaStringToHsva(rgbaStr);
+    onChange && onChange(handleColor(newHsva), evn);
   };
+
   return (
     <div
       ref={ref}
@@ -30,23 +74,24 @@ const Slider = React.forwardRef<HTMLDivElement, SliderProps>((props, ref) => {
       className={[prefixCls, className || ''].filter(Boolean).join(' ')}
       {...other}
     >
-      {lightness.map((num, idx) => {
-        const hsl = hsvaToHsla(hsva);
-        const hslStr = `hsl(${hsl.h}, 50%, ${num}%)`;
-        const checked = hslStr === hsvaToHslString(hsva);
+      {activeLightness.map((num: number, idx: number) => {
+        const newHsva = { ...hsva, v: num };
+        const rgba = hsvaToRgba(newHsva);
+        const rgbaStr = `rgba(${rgba.r}, ${rgba.g}, ${rgba.b}, ${rgba.a})`;
+        const checked = rgbaStr === hsvaToRgbaString(hsva);
         return (
           <div
             key={idx}
             style={{
               paddingLeft: 1,
-              width: `${100 / lightness.length}%`,
+              width: `${100 / activeLightness.length}%`,
               boxSizing: 'border-box',
             }}
           >
             <div
-              onClick={(evn) => handleClick(hslStr, evn)}
+              onClick={(evn) => handleClick(rgbaStr, evn)}
               style={{
-                backgroundColor: hslStr,
+                backgroundColor: rgbaStr,
                 height: 12,
                 cursor: 'pointer',
                 ...(checked
